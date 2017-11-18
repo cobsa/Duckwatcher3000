f5620ff3649455b960f30908ac51d07fdb2eb8cd
Jaakko Koponen <cobsa89@gmail.com>
2017-11-19 00:42:58 +0200

    Code reorganizing and user input handling

---
+++ src/components/form/textInput.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default class TextInput extends React.Component {
+  render() {
+    let valid = ''
+    if (this.props.valid != undefined) {
+      valid = this.props.valid ? 'is-valid' : 'is-invalid'
+    }
+
+    let classes = 'form-control ' + valid
+    return (
+      <div className="form-group">
+        <label htmlFor={this.props.id}>{this.props.name}</label>
+        <input
+          type={this.props.type}
+          className={classes}
+          id={this.props.id}
+          placeholder={this.props.placeholder}
+          value={this.props.value}
+          onChange={this.props.onChange}
+        />
+        <div className="invalid-feedback">{this.props.errorMessage}</div>
+      </div>
+    )
+  }
+}
--- src/pages/addSighting.js
+++ src/pages/addSighting.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
+import { connect } from 'react-redux'
 
 import 'react-datepicker/dist/react-datepicker.css'
 import { addSightingAction } from '../redux/actions/sightingsActions'
-import { connect } from 'react-redux'
+import { GETSPECIES } from '../redux/constants/species'
+import { FETCHED } from '../redux/constants/sightings'
+
+import TextInput from '../components/form/textInput'
 
 const mapStateToProps = state => {
   return {
@@ -16,6 +20,9 @@ const mapDispatchToProps = dispatch => {
   return {
     addSightingAction: (species, dateTime, description, count) => {
       dispatch(addSighting(species, dateTime, description, count))
+    },
+    getSpecies: () => {
+      dispatch({ type: GETSPECIES })
     }
   }
 }
@@ -23,92 +30,122 @@ const mapDispatchToProps = dispatch => {
 class AddSightingComponent extends React.Component {
   constructor() {
     super()
-    this.state = {
-      startTime: moment(),
-      species: [
-        {
-          name: 'Mallard'
-        },
-        {
-          name: 'Mallard1'
-        },
-        {
-          name: 'Mallard2'
-        },
-        {
-          name: 'Mallard3'
-        }
-      ]
-    }
     this.handleDateTimeChange = this.handleDateTimeChange.bind(this)
     this.handleDescription = this.handleDescription.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleCount = this.handleCount.bind(this)
+    this.state = {
+      startTime: moment(),
+      description: {
+        value: '',
+        valid: undefined
+      },
+      count: {
+        value: undefined,
+        valid: undefined
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.props.getSpecies()
   }
 
   handleDescription(e) {
     e.preventDefault()
-    // TODO: Implement
+    const { value } = e.target
+    let valid = false
+    if (value.length > 0) {
+      valid = true
+    }
+    this.setState({
+      description: {
+        value,
+        valid
+      }
+    })
   }
+
+  handleCount(e) {
+    e.preventDefault()
+    const { value } = e.target
+    let valid = false
+    if (value > 0) {
+      valid = true
+    }
+    this.setState({
+      count: {
+        value,
+        valid
+      }
+    })
+  }
+
   handleDateTimeChange(date) {
-    console.log('Triggered')
     this.setState({
       startTime: date
     })
   }
 
-  handleSubmit(event) {}
+  handleSubmit(event) {
+    this.props.addSighting()
+  }
 
   render() {
-    // Populate drop down selection with duck names
-    const { species } = this.state
-    const speciesOptions = species.map(specie => {
-      return <option key={specie.name}>{specie.name}</option>
-    })
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="speciesSelect">Select species</label>
-          <select className="form-control" id="speciesSelect">
-            {speciesOptions}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputCount">Count</label>
-          <input
-            type="number"
-            className="form-control"
+    const { species, fetched } = this.props.species
+    if (fetched) {
+      // Populate drop down selection with duck names
+      const speciesOptions = species.map(specie => {
+        return <option key={specie.name}>{specie.name}</option>
+      })
+      return (
+        <form onSubmit={this.handleSubmit} noValidate>
+          <div className="form-group">
+            <label htmlFor="speciesSelect">Select species</label>
+            <select className="form-control" id="speciesSelect">
+              {speciesOptions}
+            </select>
+          </div>
+          <TextInput
+            name="Count"
             id="inputCount"
-            placeholder="Number of sighted birds"
+            type="number"
+            placeholder="Input amount of ducks sighted"
+            valid={this.state.count.valid}
+            errorMessage="Input amount that is greater than zero"
+            onChange={this.handleCount}
+            value={this.state.count.value}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputDescription">Description</label>
-          <input
-            type="text"
-            className="form-control"
+          <TextInput
+            name="Description"
             id="inputDescription"
-            placeholder="Short description about sighting"
+            type="text"
+            placeholder="Short description of sighting"
+            errorMessage="Description can't be empty"
+            value={this.state.description.value}
             onChange={this.handleDescription}
+            valid={this.state.description.valid}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputDateTime">Time</label>
-          <DatePicker
-            selected={this.state.startTime}
-            onChange={this.handleDateTimeChange}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="LLL"
-            className="form-control"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit sighting
-        </button>
-      </form>
-    )
+          <div className="form-group">
+            <label htmlFor="inputDateTime">Time</label>
+            <DatePicker
+              selected={this.state.startTime}
+              onChange={this.handleDateTimeChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="LLL"
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit sighting
+          </button>
+        </form>
+      )
+    } else {
+      return <div>Loading</div>
+    }
   }
 }
 
